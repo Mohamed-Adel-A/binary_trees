2b86138d9b4f76f0691633edd0ca54953e8e8cae
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-30 10:11:18 +0300

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,5 +13,14 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	
+	func(tree);
+	if (tree->left != NULL)
+	{
+		binary_tree_preorder(tree->left);
+	}
+
+	if (tree->right != NULL)
+	{
+		binary_tree_preorder(tree->right);
+	}
 }
