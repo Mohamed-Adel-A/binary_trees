50b786b28b55e4873d089ba852da0050f31581fe
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-31 00:04:41 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,17 +9,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	binary_tree_t *current = tree;
-	size_t max, left_ret = 0, right_ret = 0;
+	binary_tree_t *current;
+	size_t left_ret = 0, right_ret = 0;
 
 	if (tree == NULL)
 		return (0);
 
+	current = (binary_tree_t *) tree;
 	if (current->left != NULL)
 	{
 		left_ret = binary_tree_height(current->left);
 		left_ret += 1;
 	}
+
 	if (current->right != NULL)
 	{
 		right_ret = binary_tree_height(current->right);
@@ -29,5 +31,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (left_ret >= right_ret)
 		return (left_ret);
 	else
-		return (right_left);
+		return (right_ret);
 }
