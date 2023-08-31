bfd7c755f0662ad6ded023207e9c83f09c0f60f2
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-31 12:14:51 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -55,8 +55,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		right = (int) binary_tree_height(tree->right) + 1;
 	printf("left: %d\n", left);
 	printf("right: %d\n", right);
-	balance_factor = binary_tree_height(tree->left)
-	             - binary_tree_height(tree->right);
+
+	balance_factor = left - right;
 
 	return (balance_factor);
 }
